acfca1c6993aef7f37db34cc34b23c61e4e9816e
신형기 <tkaqkeldk@hanatour.com>
2022-01-15 19:33:11 +0900

    [TestDive] part1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
+  // 6: 48
   return (
-    <div className="bg-red-500">
-      <h1 className="text-blue-500">it works</h1>
+    <div className="bg-slate-400 py-10 px-5 flex flex-col space-y-5">
+      <div className="bg-white p-10 rounded-3xl mb-10"></div>
+      <div className="bg-white p-10 rounded-3xl mb-10"></div>
+      <div className="bg-white p-10 rounded-3xl mb-10"></div>
+      <div className="bg-white p-10 rounded-3xl mb-10"></div>
     </div>
   );
 };
