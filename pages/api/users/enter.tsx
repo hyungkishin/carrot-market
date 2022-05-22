51131f36402d02df21c97c531843c17a3d236a46
hyungkiShin <tkaqkeldk@nate.com>
2022-05-23 00:13:54 +0900

    feat account logic 추가

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -2,14 +2,23 @@ import { NextApiRequest, NextApiResponse } from "next";
 import client from "@libs/client/client";
 import withHandler from "@libs/server/withHandler";
 
-async function handler (
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+async function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log(req.body);
+  const { phone, email } = req.body;
+  const payload = phone ? { phone: +phone } : { email };
+  const user = await client.user.upsert({
+    where: {
+      ...payload,
+    },
+    create: {
+      name: "Anonymous",
+      ...payload,
+    },
+    update: {},
+  });
+  console.log(user);
   // res.json({ ok: true });
   return res.status(200).end();
 }
 
 export default withHandler("POST", handler);
-
