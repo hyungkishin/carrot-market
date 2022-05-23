c6c83b620028ce13faa4a3c9782d6d76651f9ae7
hyungkiShin <tkaqkeldk@nate.com>
2022-05-24 00:20:59 +0900

    feat Twilio sms develop

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,6 +1,9 @@
+import twilio from "twilio";
 import { NextApiRequest, NextApiResponse } from "next";
 import client from "@libs/client/client";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
+
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
 async function handler(
   req: NextApiRequest,
@@ -25,6 +28,14 @@ async function handler(
       },
     },
   });
+  if(phone) {
+    const message = await twilioClient.messages.create({
+      messagingServiceSid: process.env.TWILIO_MSID,
+      to: `82${process.env.MY_PHONE}`!,
+      body: `Your login token is ${payload}.`
+    })
+    console.log(message);
+  }
   return res.json({
     ok: true,
   });
